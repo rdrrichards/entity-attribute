c173767d3d11f671d25ae80be5654ae7931ea4ac
R Richards <rdrrichards@hotmail.com>
2017-12-31 14:12:27 -0600

    unit test coverage added

--- src/attribute-list/attribute-list.component.ts
+++ src/attribute-list/attribute-list.component.ts
@@ -3,8 +3,14 @@ import { Attribute } from '../attribute';
 
 @Component({
   selector: 'app-attribute-list',
-  templateUrl: './attribute-list.component.html',
-  styleUrls: ['./attribute-list.component.css']
+  template: `<p-dataTable [value]="attributes" class="ui-g-12">
+    <p-column field="attributeId" header="Attribute Id" hidden="hidden"></p-column>
+    <p-column field="attributeType.attributeTypeName" header="Attribute Type Name"></p-column>
+    <p-column field="attributeValue" header="Attribute Value"></p-column>
+    <p-column field="attributeDataType" header="Data Type"></p-column>
+    <p-column field="attributeDisplayFormat" header="Display Format"></p-column>
+    <p-column field="attributeType.attributeTypeDesc" header="Description"></p-column>
+  </p-dataTable>`
 })
 export class AttributeListComponent {
   @Input() attributes: Attribute[] = [];
